5b5b69cdc9b1b92355e4771c6aede926a0496d62
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-19 20:52:00 -0400

    Initial commit

---
+++ o2h.ts
@@ -0,0 +1,8 @@
+export function o2h(obj: any, encodeAndWrite: (s: string) => void){
+    const o2hInstance = new O2H()
+}
+export class O2H {
+    constructor(public obj: any, public encodeAndWrite: (s: string) => void) {
+        encodeAndWrite.
+    }
+}
