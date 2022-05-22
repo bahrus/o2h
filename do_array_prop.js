d7a87b87d050384ad984b819f4158b71f8478c12
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-22 11:22:45 -0400

    Initial commit

--- do_array_prop.js
+++ do_array_prop.js
@@ -3,6 +3,9 @@ export async function do_array_prop({ self, config }, srcObj, prop) {
     const val = srcObj[prop];
     const { arrayPropClose, arrayPropOpen } = config;
     self.encodeAndWrite(arrayPropOpen.replaceAll('$0', self.propString(prop)));
-    await self.do_object(self, val);
+    for (let i = 0, len = val.length; i < len; i++) {
+        await self.do_prop(self, val, i);
+    }
+    //await self.do_object(self, val);
     self.encodeAndWrite(arrayPropClose);
 }
--- do_array_prop.ts
+++ do_array_prop.ts
@@ -1,9 +1,12 @@
 import {o2h, O2H} from './o2h.js';
 export async function do_array_prop({self, config}: O2H, srcObj: any, prop: string | number){
     console.log('do_array_prop');
-    const val = srcObj[prop];
+    const val = srcObj[prop] as any[];
     const {arrayPropClose, arrayPropOpen} = config;
     self.encodeAndWrite(arrayPropOpen.replaceAll('$0', self.propString(prop)));
-    await self.do_object(self, val);
+    for(let i = 0, len = val.length; i < len; i++){
+        await self.do_prop(self, val, i);
+    }
+    //await self.do_object(self, val);
     self.encodeAndWrite(arrayPropClose);
 }
