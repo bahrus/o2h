fc4c7b40f44a8605296f8bcc4f850d9ab61c6e12
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-22 19:11:03 -0400

    Initial commit

--- do_array_item.js
+++
@@ -1,7 +0,0 @@
-export async function do_array_item({ self, config }, srcObj, idx) {
-    const val = srcObj[idx];
-    const { arrayItemClose, arrayItemOpen } = config;
-    self.encodeAndWrite(arrayItemOpen.replaceAll('$0', idx.toString()));
-    await self.do_object(self, val);
-    self.encodeAndWrite(arrayItemClose);
-}
--- do_array_item.ts
+++
@@ -1,8 +0,0 @@
-import {o2h, O2H} from './o2h.js';
-export async function do_array_item({self, config}: O2H, srcObj: any, idx: number){
-    const val = srcObj[idx];
-    const {arrayItemClose, arrayItemOpen} = config;
-    self.encodeAndWrite(arrayItemOpen.replaceAll('$0', idx.toString()));
-    await self.do_object(self, val);
-    self.encodeAndWrite(arrayItemClose);
-}
