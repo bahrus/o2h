8ed871e5302807fb5d6176270091b091f7f4f217
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-27 17:20:33 -0400

    0.0.1

--- types.d.ts
+++ types.d.ts
@@ -1,20 +1,32 @@
+import {MayItBe} from 'may-it-be/types';
+
 export interface O2HConfig {
     wrapperOpen: string;
     wrapperClose: string;
     objectPropOpen: string;
+    objectPropBeProps: MayItBe; //TODO
     objectPropClose: string;
     stringPropOpen: string;
+    stringPropBeProps: MayItBe; //TODO
     stringPropValue: string;
     stringPropClose: string;
     numberPropOpen: string;
+    numberPropBeProps: MayItBe; //TODO
     numberPropValue: string;
     numberPropClose: string;
+    datePropOpen: string; //TODO
+    datePropBeProps: MayItBe; //TODO
+    datePropValue: string;
+    datePropClose: string;
     booleanPropOpen: string;
+    booleanPropBeProps: MayItBe; //TODO
     booleanPropValue: string;
     booleanPropClose: string;
     arrayPropOpen: string;
+    arrayPropBeProps: MayItBe; //TODO
     arrayPropClose: string;
     arrayItemOpen: string;
     arrayItemClose: string;
     rootConfig?: O2HConfig;
+    pathOverrides?: {[key: string]: O2HConfig};//TODO
 }
