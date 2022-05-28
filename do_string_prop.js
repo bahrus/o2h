bd72de3b9617445da0133cf29f82e1aa33be01e3
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-28 11:59:23 -0400

    0.0.1

--- do_string_prop.js
+++ do_string_prop.js
@@ -1,9 +1,10 @@
 import { replMIB } from './o2h.js';
 export async function do_string_prop({ self, contextualConfig, stack }, srcObj, prop) {
-    console.log(stack.join('.'));
+    //console.log(stack.join('.'));
     const val = srcObj[prop];
     const { stringPropClose, stringPropOpen, stringPropValue, stringPropBeProps } = contextualConfig;
+    const fullyQualifiedPath = stack.join('.');
     self.encodeAndWrite(replMIB(stringPropOpen, stringPropBeProps).replaceAll('$0', self.propString(prop)));
-    self.encodeAndWrite(stringPropValue.replaceAll('$0', val));
+    self.encodeAndWrite(stringPropValue.replaceAll('$0', val).replaceAll('$1', fullyQualifiedPath));
     self.encodeAndWrite(stringPropClose);
 }
--- do_string_prop.ts
+++ do_string_prop.ts
@@ -1,10 +1,11 @@
 import {O2H, replMIB} from './o2h.js';
 export async function do_string_prop({self, contextualConfig, stack}: O2H, srcObj: any, prop: string | number){
-    console.log(stack.join('.'));
+    //console.log(stack.join('.'));
     const val = srcObj[prop];
     const {stringPropClose, stringPropOpen, stringPropValue, stringPropBeProps} = contextualConfig;
+    const fullyQualifiedPath= stack.join('.');
     self.encodeAndWrite(replMIB(stringPropOpen, stringPropBeProps).replaceAll('$0', self.propString(prop)));
-    self.encodeAndWrite(stringPropValue.replaceAll('$0', val));
+    self.encodeAndWrite(stringPropValue.replaceAll('$0', val).replaceAll('$1', fullyQualifiedPath));
     self.encodeAndWrite(stringPropClose);
 
 }
