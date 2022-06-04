a7b99872a65a93a4c9ba26980901ac37f8146879
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-06-04 15:41:42 -0400

    0.0.3

--- do_boolean_prop.ts
+++ do_boolean_prop.ts
@@ -2,8 +2,8 @@ import {O2H, replMIB} from './o2h.js';
 export async function do_boolean_prop({self, contextualConfig}: O2H, srcObj: any, prop: string | number){
     const val = srcObj[prop];
     const {booleanPropClose, booleanPropOpen, booleanPropValue, booleanPropBeProps} = contextualConfig;
-    self.encodeAndWrite(replMIB(booleanPropOpen, booleanPropBeProps).replaceAll('$0', self.propString(prop, val)));
-    self.encodeAndWrite(booleanPropValue.replaceAll('$0', val ? 'checked' : ''));
+    self.encodeAndWrite(replMIB(booleanPropOpen, booleanPropBeProps).replaceAll('${label}', self.propString(prop, val)));
+    self.encodeAndWrite(booleanPropValue.replaceAll('${checked}', val ? 'checked' : ''));
     self.encodeAndWrite(booleanPropClose);
 
 }
