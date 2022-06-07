40ab10e4793f82d029a14025809a9878f1d20441
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-06-07 07:13:38 -0400

    0.0.3

--- types.d.ts
+++ types.d.ts
@@ -4,21 +4,17 @@ export interface O2HConfig {
     wrapper: [string, string];
     objectProp: string[];
     stringProp: string;
-    stringPropValue: string;
     numberProp: string;
-    numberPropValue: string;
-    datePropOpen: string; 
-    datePropValue: string;
-    datePropClose: string;
+    dateProp: string; 
     booleanProp: string;
     arrayProp: string;
-    arraySummaryPath: string;
-    arrayItemOpen: string;
-    arrayItemClose: string;
-    rootConfig?: O2HConfig;
     pathOverrides?: {[key: string]: O2HConfig};
     objMatchOverrides?: [{ //TODO
         ifAllOf: string[],
         then: O2HConfig
-    }]
+    }],
+    strMatchOverrides?: [{ //TODO
+        regExp: string | RegExp,
+        then: O2HConfig // based on captured groups
+    }],
 }
