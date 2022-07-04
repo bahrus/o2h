d3325d09522fa392d76bfd9bc126ef8193a10f11
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-07-04 15:23:34 -0400

    0.0.8

--- o2h.ts
+++ o2h.ts
@@ -24,10 +24,6 @@ export class O2H extends EventTarget{
     }
 
     async do_root({config, encodeAndWrite, self}: this, srcObj: any){
-        console.log({
-            method:'do_root',
-            config,
-        })
         if(config === undefined){
             const configJSON = await import('./config.json', {assert: {type: 'json'}});
             config = configJSON.default;
@@ -40,20 +36,11 @@ export class O2H extends EventTarget{
     }
 
     async do_object({}: this, obj: any){
-        console.log({
-            method: 'do_object',
-            obj
-        });
         const {do_object} = await import('./do_object.js');
         await do_object(this, obj);
     }
 
     async do_prop({stack, config}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_prop',
-            obj,
-            prop
-        });
         const {do_prop} = await import('./do_prop.js');
         stack.push(prop);
         this.contextualConfig = config.pathOverrides?.[stack.join('.')] ?? config;
@@ -62,51 +49,26 @@ export class O2H extends EventTarget{
     }
 
     async do_string_prop({stack}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_string_prop',
-            obj,
-            prop
-        });
         const {do_string_prop} = await import('./do_string_prop.js');
         await do_string_prop(this, obj, prop);
     }
 
     async do_boolean_prop({stack}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_boolean_prop',
-            obj,
-            prop
-        });
         const {do_boolean_prop} = await import('./do_boolean_prop.js');
         await do_boolean_prop(this, obj, prop);
     }
 
     async do_number_prop({stack}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_number_prop',
-            obj,
-            prop
-        });
         const {do_number_prop} = await import('./do_number_prop.js');
         await do_number_prop(this, obj, prop);
     }
 
     async do_object_prop({stack}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_object_prop',
-            obj,
-            prop
-        });
         const {do_object_prop} = await import('./do_object_prop.js');
         await do_object_prop(this, obj, prop);
     }
 
     async do_array_prop({stack}: this, obj: any, prop: string | number){
-        console.log({
-            method: 'do_array_prop',
-            obj,
-            prop
-        });
         const {do_array_prop} = await import('./do_array_prop.js');
         await do_array_prop(this, obj, prop);
     }
