a0c8d91970a8f7f0db6d3dadce34eca0b9b169eb
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-07-16 16:05:09 -0400

    0.0.10

--- do_object_prop.js
+++ do_object_prop.js
@@ -1,11 +1,12 @@
 import { titleCase } from './o2h.js';
 import { o2a } from './o2a.js';
-export async function do_object_prop({ self, contextualConfig, encodeAndWrite }, srcObj, prop) {
+export async function do_object_prop({ self, contextualConfig, encodeAndWrite, stack }, srcObj, prop) {
     const { objectProp, makeObjectPropLabelTitleCase } = contextualConfig;
     const val = srcObj[prop];
     let label = self.propString(prop, val);
     if (makeObjectPropLabelTitleCase)
         label = titleCase(label);
+    const dashedPath = stack.join('-');
     let isStatic = true;
     for (const part of objectProp) {
         if (isStatic) {
@@ -22,6 +23,9 @@ export async function do_object_prop({ self, contextualConfig, encodeAndWrite },
                         break;
                     case 'label':
                         encodeAndWrite(label);
+                        break;
+                    case 'dashed-path':
+                        encodeAndWrite(dashedPath);
                         break;
                     default:
                         throw 'NI'; //not implemented
--- do_object_prop.ts
+++ do_object_prop.ts
@@ -1,10 +1,11 @@
 import {o2h, O2H, titleCase} from './o2h.js';
 import {o2a} from './o2a.js';
-export async function do_object_prop({self, contextualConfig, encodeAndWrite}: O2H, srcObj: any, prop: string | number){
+export async function do_object_prop({self, contextualConfig, encodeAndWrite, stack}: O2H, srcObj: any, prop: string | number){
     const {objectProp, makeObjectPropLabelTitleCase} = contextualConfig;
     const val = srcObj[prop];
     let label =  self.propString(prop, val);
     if(makeObjectPropLabelTitleCase) label = titleCase(label);
+    const dashedPath= stack.join('-');
     let isStatic = true;
     for(const part of objectProp){
         if(isStatic){
@@ -21,6 +22,9 @@ export async function do_object_prop({self, contextualConfig, encodeAndWrite}: O
                         break;
                     case 'label':
                         encodeAndWrite(label);
+                        break;
+                    case 'dashed-path':
+                        encodeAndWrite(dashedPath);
                         break;
                     default:
                         throw 'NI'; //not implemented
